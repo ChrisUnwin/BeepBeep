3995883778008eb8f070eade1c4489ff229192cb
Chris Unwin <Chris.Unwin@red-gate.com>
2020-06-30 15:36:59 +0100

    Making change for Ryans WI#654

--- Tables/Payroll.Department.sql
+++ Tables/Payroll.Department.sql
@@ -2,7 +2,8 @@ CREATE TABLE [Payroll].[Department]
 (
 [ID] [int] NOT NULL,
 [DepartmentCode] [nvarchar] (10) COLLATE Latin1_General_CI_AS NOT NULL,
-[DepartmentName] [nvarchar] (150) COLLATE Latin1_General_CI_AS NOT NULL
+[DepartmentName] [nvarchar] (150) COLLATE Latin1_General_CI_AS NOT NULL,
+[DepartmentLocation] [nvarchar] (150) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [Payroll].[Department] ADD CONSTRAINT [PK__Departme__3214EC27B91D5BF7] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
--- Tables/dbo.OtherSharedTable.sql
+++ Tables/dbo.OtherSharedTable.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[OtherSharedTable]
 (
 [ID] [int] NOT NULL,
 [NameOfModule] [nvarchar] (50) COLLATE Latin1_General_CI_AS NOT NULL,
-[SomeFreeTextDesc] [nvarchar] (200) COLLATE Latin1_General_CI_AS NULL
+[SomeFreeTextDesc] [nvarchar] (200) COLLATE Latin1_General_CI_AS NULL,
+[HappyNewColumn] [nvarchar] (50) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[OtherSharedTable] ADD CONSTRAINT [PK__OtherSha__3214EC27BC709E7E] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
