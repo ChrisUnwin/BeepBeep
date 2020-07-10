4f14887de9e9c23ca452bc022ed87e5ab8e8395b
Chris Unwin <Chris.Unwin@red-gate.com>
2020-07-10 15:41:13 +0100

    Modified Employee Table :)

--- Tables/Payroll.Employee.sql
+++ Tables/Payroll.Employee.sql
@@ -5,7 +5,8 @@ CREATE TABLE [Payroll].[Employee]
 [LastName] [nvarchar] (150) COLLATE Latin1_General_CI_AS NOT NULL,
 [DateOfBirth] [datetime] NOT NULL,
 [PayrollNumber] [int] NOT NULL,
-[DepartmentID] [int] NOT NULL
+[DepartmentID] [int] NOT NULL,
+[Twitter] [nvarchar] (50) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [Payroll].[Employee] ADD CONSTRAINT [PK__Employee__3214EC27C7754DAD] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
